989f225fdbd5ecc43935763bbc1a3b6e7b3e1595
howardko <howardko@qnap.com>
2021-10-17 23:56:03 +0800

    hardcoded workaround for heroku deployment - process.env.HEROKU_APP_NAME not set

--- client/src/configs/network.ts
+++ client/src/configs/network.ts
@@ -2,7 +2,7 @@ const port = parseInt(process.env.PORT, 10) || 8080;
 
 const host = ():string => {
     if (process.env.NODE_ENV === 'production') {
-        return `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+        return `https://howard-simple-chatroom.herokuapp.com`
     }
     return `http://localhost:${port}`
 }
